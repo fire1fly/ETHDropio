f6deb60c5b952191de396e8a6ecf31ab30a2dcb7
fire1fly <danyseidev@gmail.com>
2023-08-14 03:46:17 +0800

    chore: add translations

--- src/shared/config/i18n/en.ts
+++ src/shared/config/i18n/en.ts
@@ -1,17 +1,21 @@
+/* eslint-disable max-len */
 const en = {
   translation: {
     howToGet: 'How to get?',
+    howToStart: 'How to get started?',
     connect: 'Connect',
-    pageTitle: "Don't miss the chance to get free coins through airdrop",
-    pageSubtitle: 'Join us and follow the simple steps to become a member',
+    connectWallet: 'Connect wallet',
+    pageTitle_1: 'Celebrate with us the 8th anniversary of',
+    pageTitle_2: 'Etherium (ETH)',
+    pageTitle_3: 'and get awards',
+    pageSubtitle: 'Join the celebration of the anniversary of the Ether with our tasks and get a chance to win valuable awards',
     get: 'Get',
     howItWork: 'How does it work?',
     steps: {
-      title: 'How do I get free coins?',
-      subtitle: 'Join us and follow the simple steps to become a member',
-      wallet: 'Connect your wallet',
-      facebook: 'Subscribe to us on Facebook',
-      twitter: 'Follow us on Twitter',
+      step_1: 'Subscribe to the official Twitter of ETH',
+      step_2: 'Connect wallet',
+      step_3: 'Make a post about getting a free coin (screenshot)',
+      footer: 'Description that everything is safe, etc.',
     },
     subscribe: 'Subscribe',
     joinus: 'Join us',
