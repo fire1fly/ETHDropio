7a11cf62f63eb7c36464078db0d36b9b81f60837
fire1fly <danyseidev@gmail.com>
2023-08-14 03:53:56 +0800

    feat(pages): add MainPage

---
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -0,0 +1,30 @@
+import { Header } from 'widgets/Header';
+import { MainScreen } from 'widgets/MainScreen';
+import { Steps } from 'widgets/Steps';
+import { useRef } from 'react';
+import cls from './MainPage.module.scss';
+
+const MainPage = () => {
+  const stepsEl = useRef<HTMLSelectElement>(null);
+
+  const handleScrollToSteps = () => {
+    const topOffset = stepsEl?.current?.offsetTop;
+
+    window.scrollTo({
+      top: topOffset,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div className={cls.Page}>
+      <div className={cls.Page_inner}>
+        <Header onClickScroll={handleScrollToSteps} />
+        <MainScreen onClickScroll={handleScrollToSteps} />
+        <Steps ref={stepsEl} />
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
